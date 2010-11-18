7908b41ad9df1d5169faa1c003cd6f1a000c0c28
Marc Diethelm <boo@chic-happens.ch>
2010-11-18 10:10:33 +0100

    Improved the introduction text in the js file.

--- jquery.shorten.js
+++ jquery.shorten.js
@@ -8,66 +8,64 @@
 
 
 /****************************************************************************
- * jQuery 1.3.x plugin to shorten styled text to fit in a block, appending
- * an ellipsis ("...", &hellip;, Unicode: 2026) or other text.
- * (Only supports ltr text for now.)
- *
- * This is achieved by placing the text of the 'selected' element (eg. span or
- * div) inside a table and measuring its width. If it's too big to big to fit in
- * the element's parent block it's shortened and measured again until it (and
- * appended ellipsis or text) fits inside the block. A tooltip on the 'selected'
- * element displays the full original text.
- *
- * If the browser supports truncating text using the 'text-overflow:ellipsis'
- * CSS property then that will be used (if the text to append is the default
- * ellipsis).
- *
- * If the text is truncated by the plugin any markup in the text will be
- * stripped (eg: "<a" starts stripping, "< a" does not). This behaviour is
- * dictated by the jQuery .text(val) method.
- * The appended text may contain HTML however (a link or span for example).
- *
- * Usage Example ('selecting' a div with an id of "element"):
+This jQuery plugin automatically shortens text to fit in a block or pre-set width while you can configure how the text ends. The default is an ellipsis  ("…", &hellip;, Unicode: 2026) but you can use anything you want, including markup.
 
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-	<script type="text/javascript" src="jquery.shorten.js"></script>
-	<script type="text/javascript">
-		$(function() {
-			$("#element").shorten();
-		});
-	</script>
+This is achieved using either of two methods: First the the text width of the 'selected' element (eg. span or div) is measured using Canvas or by placing it inside a temporary table cell. If it's too big to big to fit in the element's parent block it is shortened and measured again until it (and the appended ellipsis or text) fits inside the block. A tooltip on the 'selected' element displays the full original text.
 
- * By default the plugin will use the parent block's width as maximum width and
- * an ellipsis as appended text when truncating.
- *
- * There are three ways of configuring the plugin:
- *
- * 1) Passing a configuration hash as the plugin's argument, eg:
+If the browser supports truncating text with CSS ('text-overflow:ellipsis') then that is used (but only if the text to append is the default ellipsis).
 
-	.shorten({
-		width: 300,
-		tail: ' <a href="#">more</a>',
-		tooltip: false
-	});
+If the text is truncated by the plugin any markup in the text will be stripped (eg: "<a" starts stripping, "< a" does not). This behaviour is dictated by the jQuery .text(val) method. The appended text may contain HTML however (a link or span for example).
 
- * 2) Using two optional arguments (deprecated!):
- * width = the desired pixel width, integer
- * tail = text/html to append when truncating
- *
- * 3) By changing the plugin defaults, eg:
 
-	$.fn.shorten.defaults.tail = ' <a href="#">more</a>';
+Usage Example ('selecting' a div with an id of "element"):
 
- * Note: there is no default width (unless you create one).
- *
- * You may want to set the element's css to {visibility:hidden;} so it won't
- * initially flash at full width.
- *
- *
- * Based on a creation by M. David Green (www.mdavidgreen.com) in 2009.
- *
- * Heavily modified/simplified/improved by Marc Diethelm (http://web5.me/).
- *
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+<script type="text/javascript" src="jquery.shorten.js"></script>
+<script type="text/javascript">
+    $(function() {
+        $("#element").shorten();
+    });
+</script>
+
+
+By default the plugin will use the parent block's width as the maximum width and an ellipsis as appended text when the text is truncated.
+
+There are three ways of configuring the plugin:
+
+1) Passing a configuration hash as the plugin's argument, eg:
+
+.shorten({
+    width: 300,
+    tail: ' <a href="#">more</a>',
+    tooltip: false
+});
+
+2) Using two optional arguments (deprecated!):
+width = the desired pixel width, integer
+tail = text/html to append when truncating
+
+3) By changing the plugin defaults, eg:
+
+
+$.fn.shorten.defaults.tail = ' <a href="#">more</a>';
+
+
+Notes:
+
+There is no default width (unless you create one).
+
+You may want to set the element's CSS to {visibility:hidden;} so it won't
+initially flash at full width in slow browsers.
+
+Only supports ltr text for now.
+
+Tested with jQuery 1.3+
+
+
+Based on a creation by M. David Green (www.mdavidgreen.com) in 2009.
+
+Heavily modified/simplified/improved by Marc Diethelm (http://web5.me/).
+
 ****************************************************************************/
 }
 
